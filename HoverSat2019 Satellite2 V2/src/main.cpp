c4add7fa0d88d33ad8f3bdfe2b16b4164f2bc2cd
KaishuKoike <50565970+KaishuKoike@users.noreply.github.com>
2019-11-10 17:05:33 +0900

    1

--- HoverSat2019 Satellite2 V2/src/main.cpp
+++ HoverSat2019 Satellite2 V2/src/main.cpp
@@ -473,7 +473,7 @@ void Timer_Interrupt( void ){
         M5.Lcd.print("No.");
         M5.Lcd.setTextSize(5);
         M5.Lcd.setCursor(28, 160);
-        M5.Lcd.print(patternNo+1);
+        M5.Lcd.printf("%2d", patternNo+1);
 
         M5.Lcd.setTextSize(2);
         M5.Lcd.setCursor(96, 92);
@@ -500,7 +500,7 @@ void Timer_Interrupt( void ){
         M5.Lcd.print("No.");
         M5.Lcd.setTextSize(5);
         M5.Lcd.setCursor(28, 160);
-        M5.Lcd.print(patternNo+1);
+        M5.Lcd.printf("%2d", patternNo+1);
 
         M5.Lcd.setTextSize(2);
         M5.Lcd.setCursor(96, 92);
